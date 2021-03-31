a7107eb39c083966f9412d1d85e7d30dfbcccdbb
Zubair <Zubair@DESKTOP-6NU3360>
2021-03-31 18:33:21 -0400

    First Commmit

---
+++ CucumberProjectNew/src/test/java/CucumberProjectNew/CucumberProjectNew/AppTest.java
@@ -0,0 +1,20 @@
+package CucumberProjectNew.CucumberProjectNew;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ CucumberProjectNew/src/test/java/com/runner/Runner.java
@@ -0,0 +1,19 @@
+package com.runner;
+
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class) //ctrl+shift+o will import junit.cucumber
+@CucumberOptions(features="src/test/resources/loginFeature.feature", glue= {"com.stepDefinition"},
+monochrome=true,
+plugin= {"html:Reports//HTMLReports", "json:Reports//JsonReports//cucumber.json", "junit:Reports//JUnitReports//cucumber.xml"},//generate report
+dryRun=false,
+strict=true
+)
+
+public class Runner {
+	
+
+}
